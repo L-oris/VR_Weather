013bef79e6a5334d81be4f5e9a5404e6c7e5919c
Loris <loris4526@gmail.com>
2017-11-07 13:54:54 +0100

    Fetch weather data from 'openweathermap' API, and set into app state

--- index.vr.js
+++ index.vr.js
@@ -1,34 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react'
 import {
   AppRegistry,
   asset,
-  Pano,
-  Text,
   View,
-} from 'react-vr';
+  Pano,
+} from 'react-vr'
 
-export default class VRWeather extends React.Component {
-  render() {
+const {API_KEY} = require('./secret.json')
+
+class VRWeather extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
+
+  componentDidMount(){
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${API_KEY}`,{
+      method: 'GET'
+    })
+    .then(apiResponse => apiResponse.json())
+    .then(json => {
+      this.setState({weather: json})
+    })
+  }
+
+  render(){
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
+        <Pano source={asset('sf.jpg')}/>
       </View>
-    );
+    )
   }
-};
 
-AppRegistry.registerComponent('VRWeather', () => VRWeather);
+}
+
+
+AppRegistry.registerComponent(
+  'VRWeather',
+  ()=>VRWeather
+)
